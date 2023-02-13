ea0f12711db4320d1fcb04308060249b0cd65dc9
hanqing <602997434@qq.com>
2023-02-13 01:00:41 -0800

    合并有序数组

--- mergearrays.c
+++ mergearrays.c
@@ -23,3 +23,21 @@ void MergeArrays(int[] nums_1, int m, int[] nums_2, int n) {
         nums_1[i] = temp[i];
     }
 }
+
+void merge(int[] nums_1, int m, int[] nums_2, int n) {
+    int k = m + n;
+    for (int index = k - 1, index_1 = m-1, index_2 = n-1; index >= 0; i--) {
+        if (index_1 < 0) {
+            nums_1[index] = nums_2[index_2--];
+        }
+        else if (index_2 < 0) {
+            break;
+        }
+        else if (nums_1[index_1] < nums_2[index_2]) {
+            nums_1[index] = nums_2[index_2--];
+        }
+        else {
+            nums_1[index] = nums_1[index_1--];
+        }
+    }
+}
