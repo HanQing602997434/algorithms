6546c669f44648024450f03304628622b1edb932
hanqing <602997434@qq.com>
2023-02-17 00:37:59 -0800

    获得相交链表节点的两种解法

--- getIntersectionNode.c
+++ getIntersectionNode.c
@@ -1,6 +1,7 @@
 // 相交链表
 // 获取两个链表相交的节点
 
+// 解法一
 ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
     if (headA == NULL || headB == NULL)
         return NULL;
@@ -11,4 +12,43 @@ ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
         pB = pB == NULL ? headA : pB->next;
     }
     return pA;
+}
+
+// 解法二
+ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+    if (headA == NULL || headB == NULL)
+        return NULL;
+
+    int lenA = 0, lenB = 0;
+    ListNode* pA = headA;
+    ListNode* pB = headB;
+    
+    while (pA) {
+        lenA++;
+        pA = pA->next;
+    }
+    while (pB) {
+        lenB++;
+        pB = pB->next;
+    }
+
+    if (lenA > lenB) {
+        for (int i = 0; i < lenA-lenB; i++) {
+            pA = pA->next;
+        }
+    }
+    else {
+        for (int i = 0; i < lenB-lenA; i++) {
+            pB = pB->next;
+        }
+    }
+
+    while (pA != NULL && pB != NULL) {
+        if (pA == pB)
+            return pA;
+        pA = pA->next;
+        pB = pB->next;
+    }
+
+    return NULL;
 }
