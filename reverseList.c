bbc35e93ec482c2ce668d00691b86eaefc2c5543
hanqing <602997434@qq.com>
2023-02-19 19:07:54 -0800

    反转链表

--- reverseList.c
+++ reverseList.c
@@ -1,2 +1,18 @@
 // 反转链表
 
+ListNode* reverseList(ListNode* head) {
+    if (head == NULL)
+        return NULL;
+
+    ListNode* preNode = NULL;
+    ListNode* curNode = head;
+    ListNode* next = NULL;
+    while (curNode) {
+        next = curNode->next;
+        curNode->next = preNode;
+        preNode = curNode;
+        curNode = next;
+    }
+
+    return preNode;
+}
