70c5b68c62654338e0b8a14313f2a6003eedb887
hanqing <602997434@qq.com>
2023-02-27 21:55:24 -0800

    二叉树的后序遍历

--- postorderTraversal.c
+++ postorderTraversal.c
@@ -1,1 +1,21 @@
 // 二叉树的后序遍历
+
+TreeNode {
+    int val;
+    TreeNode* parent;
+    TreeNode* left;
+    TreeNode* right;
+}
+
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> res;
+    accessTree(root, res);
+    return res;
+}
+
+void accessTree(TreeNode* root, vector<int>& res) {
+    if (root == NULL) return;
+    accessTree(root->left);
+    accessTree(root->right);
+    res.push_back(root->val);
+}
