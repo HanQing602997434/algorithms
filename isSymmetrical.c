6b090291c98c37b979e6400994779ffa6c7c805d
hanqing <602997434@qq.com>
2023-03-01 19:22:37 -0800

    对称二叉树

--- isSymmetrical.c
+++ isSymmetrical.c
@@ -1,3 +1,33 @@
 // 对称二叉树
 
 // 给定一个二叉树，检查它是否是镜像对称的。
+
+TreeNode {
+    int val;
+    TreeNode* parent;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), parent(NULL), left(NULL), right(NULL) {}
+}
+
+bool isSymmetrical(TreeNode* root) {
+    if (root == NULL) return true;
+
+    // 调用递归函数，比较左节点，右节点
+    return deepCheck(root->left, root->right);
+}
+
+bool deepCheck(TreeNode* left, TreeNode* right) {
+    // 递归的终止条件是两个节点都为空
+    // 或者两个节点中有一个为空
+    // 或者两个节点值不相等
+    if (left == NULL && right == NULL) return true;
+
+    if (left == NULL || right == NULL) return false;
+
+    if (left->val != right->val) return false;
+
+    // 再递归的比较，左节点的左孩子 和 右节点的右孩子
+    // 以及比较 左节点的右孩子 和 右节点的左孩子
+    return deepCheck(left->left, right->right) && deepCheck(left->right, right->left);
+}
