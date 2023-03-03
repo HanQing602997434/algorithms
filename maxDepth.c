b413b07a2c074317e74a967d5ccdb88d7803f961
hanqing <602997434@qq.com>
2023-03-03 00:29:48 -0800

    二叉树的最大深度循环解法

--- maxDepth.c
+++ maxDepth.c
@@ -16,3 +16,31 @@ int maxDepth(TreeNode* root) {
         return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 }
+
+int maxDepthWithLoop(TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    queue<TreeNode*> q;
+    q.push(root);
+    int depth = 0;
+    while (!q.empty()) {
+        int size = q.size();
+        while (size > 0) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (node->left != NULL) {
+                q.push(node->left);
+            }
+            if (node->right != NULL) {
+                q.push(node->right);
+            }
+            size--;
+        }
+        depth++;
+    }
+
+    return depth;
+}
