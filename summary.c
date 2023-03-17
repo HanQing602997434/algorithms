6e44d08104c88b446ad6e2ef3618c130034a54de
hanqing <602997434@qq.com>
2023-03-17 02:00:53 -0700

    排序算法总结

--- summary.c
+++ summary.c
@@ -4,5 +4,10 @@
 冒泡排序           O(n^2)           O(n)       O(n^2)         O(1)        稳定        是
 选择排序           O(n^2)          O(n^2)      O(n^2)         O(1)       不稳定       是
 插入排序           O(n^2)           O(n)       O(n^2)         O(1)        稳定        是
-快速排序          O(nlogn)        O(nlogn)     O(n^2)        O(logn)     不稳定       是
 希尔排序       O(nlogn)~O(n^2)    O(n^1.3)     O(n^2)         O(1)       不稳定       是
+归并排序          O(nlogn)        O(nlogn)    O(nlogn)        O(n)        稳定        是
+快速排序          O(nlogn)        O(nlogn)     O(n^2)        O(logn)     不稳定       是
+堆排序            O(nlogn)        O(nlogn)    O(nlogn)        O(1)       不稳定       是
+计数排序
+桶排序
+基数排序
