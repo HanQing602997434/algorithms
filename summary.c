b89485ae2a2d9ecc320f68dd9013dae54ef610ed
hanqing <602997434@qq.com>
2023-03-19 23:00:37 -0700

    总结

--- summary.c
+++ summary.c
@@ -8,6 +8,6 @@
 归并排序          O(nlogn)        O(nlogn)    O(nlogn)        O(n)        稳定        是
 快速排序          O(nlogn)        O(nlogn)     O(n^2)        O(logn)     不稳定       是
 堆排序            O(nlogn)        O(nlogn)    O(nlogn)        O(1)       不稳定       是
-计数排序
-桶排序
+计数排序           O(n+k)          O(n+k)      O(n+k)         O(k)        稳定        否    
+桶排序             O(n+c)           O(n)       O(n^2)        O(n+k)       稳定        否
 基数排序
