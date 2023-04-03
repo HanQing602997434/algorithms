eda623f876d5f1ccb2f268e397da7644c2642022
hanqing <602997434@qq.com>
2023-04-02 23:11:12 -0700

    比特位计数

---
+++ countBit.c
@@ -0,0 +1,10 @@
+// 比特位计数
+vector<int> countBits(int num) {
+    vector<int> bits;
+    bits.resize(num + 1);
+    for (int i = 1; i < num; i++) {
+        bits[i] = bits[i & (i - 1)] + 1;
+    }
+
+    return bits;
+}
