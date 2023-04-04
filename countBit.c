3f146ccee2b7ed6a70058c364efd61033758295f
hanqing <602997434@qq.com>
2023-04-03 22:54:29 -0700

    比特位计数-利用奇偶性解法

--- countBit.c
+++ countBit.c
@@ -12,4 +12,10 @@ vector<int> countBits(int num) {
 // 利用奇偶性解决
 vector<int> countBits(int num) {
     vector<int> bits;
+    bits.resize(num + 1);
+    bits[0] = 0;
+    for (int i = 1; i <= num; i++) {
+        bits[i] = ((i&1) == 1 ? bits[i-1]+1 : bits[i>>1]);
+    }
+    return bits;
 }
