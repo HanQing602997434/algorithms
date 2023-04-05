88403716d08d7cdfbed74a7c80f9e48aaf3d114f
hanqing <602997434@qq.com>
2023-04-04 20:09:23 -0700

    汉明距离

--- hanmingDistance.c
+++ hanmingDistance.c
@@ -1,2 +1,11 @@
 // 汉明距离
 // 两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+
+int hanmingDistance(int x, int y) {
+    int distance = 0;
+    for (int xor = x ^ y; xor != 0; xor & (xor - 1)) {
+        distance++;
+    }
+    
+    return distance;
+}
