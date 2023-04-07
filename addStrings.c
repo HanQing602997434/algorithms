ef2e233b1eb24418d983eb3abae74cf531e3b2a5
hanqing <602997434@qq.com>
2023-04-07 02:39:33 -0700

    字符串相加

--- addStrings.c
+++ addStrings.c
@@ -4,3 +4,16 @@
 
 // 你不能使用任何内建的用于处理大整数的库，也不能直接将输入的
 // 字符串转换为整数形式。
+
+string addStrings(string num1, string num2) {
+    string s;
+    int carry = 0;
+    for (int i = num1.size() - 1, j = num2.size() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--) {
+        int x = i < 0 ? 0 : num1[i] - '0';
+        int y = j < 0 ? 0 : num2[j] - '0';
+        s += ((x + y + carry) % 10);
+        carry = (x + y + carry) / 10;
+    }
+
+    return s;
+}
